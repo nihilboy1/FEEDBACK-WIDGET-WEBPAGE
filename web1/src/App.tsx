4bbf2342f3eaaba2882e8de38039a36a88fd5ecf
Samuel Seve <samuelseve1@gmail.com>
2022-05-09 20:02:00 -0300

    Changes

--- web1/src/App.tsx
+++ web1/src/App.tsx
@@ -1,10 +1,3 @@
-import { useState } from 'react'
-import { Widget } from "./components/Widget"
-
-export function App() {
-  return (
-    <>
-      <Widget />
-    </>
-  )
+export function Hello() {
+  return <h1>{`Bem vindo ao meu perfil! :)`}</h1>
 }
