0ccd98fd87ec558e6ec6f9f2b8115ea6ea5ac74f
Samuel Seve <samuelseve1@gmail.com>
2022-05-12 19:11:15 -0300

    Update server.ts

--- server2/src/server.ts
+++ server2/src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import cors from "cors"
+import cors from 'cors'
 import { routes } from './routes'
 
 const app = express()
@@ -7,10 +7,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-
-
-
-
 app.listen(process.env.PORT || 3333, () => {
-  console.log('Server Runnnnnning!')
+  console.log('Server Running!')
 })
